9ef1303f0a0a66ca069975aca63d3f10d19354f3
grisorry <graysintos@gmail.com>
2023-05-10 17:09:14 +0800

    Finishing up the project

--- script.js
+++ script.js
@@ -1,1 +1,62 @@
+const getComputerChoice = () => ['rock', 'paper', 'scissors'][Math.floor(Math.random() * 3)];
+
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+    switch (playerSelection) {
+     
+        case 'rock':
+            if (computerSelection === 'paper'){
+                return 'You Lose! Paper beats Rock';
+            }else if (computerSelection === 'scissors'){
+                return 'You Win! Rock beats Scissors';
+            }else{
+                return 'Draw';
+            }
+            break;
+        case 'paper':
+            if (computerSelection === 'scissors'){
+                return 'You Lose! Scissors beats Paper';
+            }else if (computerSelection === 'rock'){
+                return 'You Win! Paper beats Rock';
+            }else{
+                return 'Draw';
+            }
+            break;
+        case 'scissors':
+            if (computerSelection === 'rock'){
+                return 'You Lose! Rock beats Scissors';
+            }else if (computerSelection === 'paper'){
+                return 'You Win! Scissors beats Paper';
+            }else{
+                return 'Draw';
+            }
+            break;
+        default:
+            return 'Invalid input';
+    }
+  }
+
+  function game(){
+        console.log(playRound(playerSelection, computerSelection));
+        if(playRound(playerSelection, computerSelection).includes('Win')){
+            userPoints++;
+        }else if(playRound(playerSelection, computerSelection).includes('Lose')){
+            computerPoints++;
+        }
+        console.log(`User: ${userPoints} Computer: ${computerPoints}`);
+  }
+  let playerSelection = prompt('Rock, Paper or Scissors?').toLowerCase();
+  const computerSelection = getComputerChoice();
+  let userPoints = 0;
+  let computerPoints = 0;
+
+  while(userPoints < 5 && computerPoints < 5){
+      game();
+      if(userPoints === 5){
+          console.log('You won the game!');
+        }else if(computerPoints === 5){
+            console.log('You lost the game!');
+        }
+      playerSelection = prompt('Rock, Paper or Scissors?').toLowerCase();
+  }
 
